9661c72d5ec2f743e59045ebacb4d6ea3ef78aa2
Iza <izabela.wlazlo@ttsw.com>
2023-06-17 17:45:09 +0200

    galeria

--- linden-photography/src/app/components/pages/home/home.module.ts
+++ linden-photography/src/app/components/pages/home/home.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
+import { PrimeNgModule } from '../../../shared/modules/primeng.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, PrimeNgModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
--- linden-photography/src/app/shared/modules/primeng.module.ts
+++ linden-photography/src/app/shared/modules/primeng.module.ts
@@ -3,8 +3,15 @@ import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { GalleriaModule } from 'primeng/galleria';
 
 @NgModule({
-  exports: [ButtonModule, DropdownModule, InputTextModule, InputTextareaModule],
+  exports: [
+    ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    InputTextareaModule,
+    GalleriaModule,
+  ],
 })
 export class PrimeNgModule {}
