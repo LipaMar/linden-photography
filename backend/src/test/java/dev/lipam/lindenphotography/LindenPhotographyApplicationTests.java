931d840d2072731ed6043a4e436678f83c1a3690
LipaMar <marcin.lipa@onet.pl>
2024-10-08 22:11:53 +0200

    change project structure- add backend initial setup

---
+++ backend/src/test/java/dev/lipam/lindenphotography/LindenPhotographyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lipam.lindenphotography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindenPhotographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
